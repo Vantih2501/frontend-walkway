ce56530a43376ee551a25a694009b0462267c7ec
Gandara <gandarafathurrhman@gmail.com>
2024-10-07 16:24:17 +0700

    feat: slicing  login page

--- app/provider.tsx
+++ app/provider.tsx
@@ -1,23 +1,25 @@
 "use client";
 
-import React, {useEffect} from "react";
-import {ConfigProvider} from "antd";
-import {TokenUtil} from "#/utils/token";
+import React, { useEffect } from "react";
+import { ConfigProvider } from "antd";
+import { TokenUtil } from "#/utils/token";
 
 TokenUtil.loadToken();
-export const Provider = ({children}: any) => {
-  // useEffect(() => {
-  //   // @ts-ignore
-  //   document.documentElement.style.opacity = 1
-  // }, []);
+export const Provider = ({ children }: any) => {
+	// useEffect(() => {
+	//   // @ts-ignore
+	//   document.documentElement.style.opacity = 1
+	// }, []);
 
-  return <ConfigProvider
-    theme={{
-      token: {
-        colorPrimary: '#00b96b',
-      },
-    }}
-  >
-    {children}
-  </ConfigProvider>
-}
+	return (
+		<ConfigProvider
+			theme={{
+				token: {
+					colorPrimary: "#4E7772",
+				},
+			}}
+		>
+			{children}
+		</ConfigProvider>
+	);
+};
