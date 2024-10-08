54837d2941c46d23e8fb167e0335bba51f56bd61
Gandara <gandarafathurrhman@gmail.com>
2024-10-08 17:20:20 +0700

    feat: add ProductCard component

--- app/(authenticated)/about/page.tsx
+++ app/(authenticated)/about/page.tsx
@@ -1,16 +1,23 @@
 "use client";
 
 import React from "react";
-import {Button, Card} from "antd";
+import {Button} from "antd";
 import {store} from "#/store";
+import ProductCard from "#/components/Card";
 
 const Page = () => {
-    return <div>
-        about: {store.ui.title}
-        <Button onClick={() => {
-            store.ui.changeTitle("from about")
-        }}>change title</Button>
-    </div>;
+    return (
+			<div>
+				about: {store.ui.title}
+				<Button
+					onClick={() => {
+						store.ui.changeTitle("from about");
+					}}
+				>
+					change title
+				</Button>
+			</div>
+		);
 };
 
 export default Page;
